5f2b25112eb5467bfc5c0956e33e5cefc7a83c41
unknown <mariia>
2019-08-22 00:37:12 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_uber-eats/precache-manifest.4831411afdd49c016f287f8976f8d4d0.js"
+  "/react_uber-eats/precache-manifest.25a25d2bb9851e35e0615aa7172fa63c.js"
 );
 
 self.addEventListener('message', (event) => {
